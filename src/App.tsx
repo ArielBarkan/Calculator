04774f64133e9e60340a93ff036b348152e016d8
Ariel Barkan <arielbarkan@gmail.com>
2025-02-12 22:49:57 +0200

    Adding support to languages (i18n) before RTL

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import React from "react";
+
 // Custom imports
 import AppRoutes from "./routes/AppRoutes";
 import { ChangeLanguage } from "./services";
 import "./services/i18nService";
+
 
 const App: React.FC = () => {
     ChangeLanguage("en-US");
