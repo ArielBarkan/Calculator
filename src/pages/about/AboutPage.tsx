b45c9385b4c71afaee7222ea46436cb2d6bd0244
Ariel Barkan <arielbarkan@gmail.com>
2025-02-12 23:52:01 +0200

    Adding buttons for toggle languages

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,11 +1,18 @@
 import { useTranslation } from "react-i18next";
 
+// Custom imports
+import { ChangeLanguage } from "../../services";
 
 const AboutPage = () => {
     const { t: translate } = useTranslation("common");
+
+
     return (
         <p>
             {translate("pages.about.title")}
+            <br />
+            <button onClick={() => ChangeLanguage("he-IL")}>Hebrew</button>
+            <button onClick={() => ChangeLanguage("en-US")}>English</button>
         </p>
     );
 };
