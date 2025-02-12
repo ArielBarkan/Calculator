9d442e9d9c37634d2792a2659fbc67cb402b5181
Ariel Barkan <arielbarkan@gmail.com>
2025-02-12 21:35:04 +0200

    Configuring prettier and tsconfig (and the relevant changes needed from this change)

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -5,7 +5,7 @@ const AppRoutes: React.FC = () => {
             <Route path="/" element={<p>Home</p>} />
             <Route path="/configuration" element={<p>Configuration</p>} />
             <Route path="/about" element={<p>About</p>} />
-            <Route path="*" element={<p>404</p>}/>
+            <Route path="*" element={<p>404</p>} />
         </Routes>
     );
 };
