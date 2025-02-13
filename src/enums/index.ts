1063d8920447d3bf24e3b4399d3b522f3e36eef8
Ariel Barkan <arielbarkan@gmail.com>
2025-02-13 21:32:06 +0200

    adding enum file

---
+++ src/enums/index.ts
@@ -0,0 +1,11 @@
+enum LANGUAGES_ENUMS {
+    "en-US" = "en-US",
+    "he-IL" = "he-IL"
+}
+
+enum THEME_ENUMS {
+    dark = "darkTheme",
+    light = "lightTheme"
+}
+
+export { THEME_ENUMS, LANGUAGES_ENUMS };
