bc4efa95f33f88a5e21cf77dce730fc76a02ed53
Ariel Barkan <arielbarkan@gmail.com>
2025-02-13 15:57:30 +0200

    COMMENT TODO ADDED

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,12 +5,13 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
     const { t: translate } = useTranslation("common");
+    // TODO: ADD "SHARE" BUTTON
     return (
         <nav>
             <Link to="/">{translate("header.linkLabels.main")}</Link>
 
             |
-            
+
             <Link to="/about">{translate("header.linkLabels.about")}</Link>
 
             |
