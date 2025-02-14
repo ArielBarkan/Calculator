5931e35453c34355cd113ca837ece116f2510dff
Ariel Barkan <arielbarkan@gmail.com>
2025-02-15 00:26:52 +0200

    style adjustments

--- src/pages/settings/SettingWrappers.tsx
+++ src/pages/settings/SettingWrappers.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Row = styled.div`
-    padding: 0 2rem;
+    padding: 1rem 2rem;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
