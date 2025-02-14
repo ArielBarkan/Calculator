1d5e3593f38703b3b812f5538cd396f19f8e518a
Ariel Barkan <arielbarkan@gmail.com>
2025-02-14 03:27:11 +0200

    preparing to Vesel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/", // Ensure proper routing
+    build: {
+        outDir: "dist" // Vercel expects output in `dist`
+    },
     server: {
         fs: {
             strict: false // Allows accessing files outside src
