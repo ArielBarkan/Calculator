e3923f09083ce5fc0133efbb8c6a21473f9370d9
Ariel Barkan <arielbarkan@gmail.com>
2025-02-14 21:00:37 +0200

    adding spanish translation

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,14 +3,19 @@ import { languageType } from "../types";
 
 const SUPPORTED_LANGUAGES: languageType[] = [
     {
-        locale: "he-IL",
-        direction: "rtl",
-        name: "Hebrew"
-    },
-    {
         locale: "en-US",
         direction: "ltr",
         name: "English"
+    },
+    {
+        locale: "es",
+        direction: "ltr",
+        name: "Spanish"
+    },
+    {
+        locale: "he-IL",
+        direction: "rtl",
+        name: "Hebrew"
     }
 
 ];
