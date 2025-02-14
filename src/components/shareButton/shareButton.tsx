e202546922b02efe09dd67c8f56d5776e7d19c33
Ariel Barkan <arielbarkan@gmail.com>
2025-02-14 19:50:06 +0200

    adding spanish translation

--- src/components/shareButton/shareButton.tsx
+++ src/components/shareButton/shareButton.tsx
@@ -3,31 +3,35 @@ import React, { useState, useEffect } from "react";
 
 // Modules imports
 import { useTranslation } from "react-i18next";
-// Custom imports
+
+
 const ShareButton: React.FC = () => {
     const { t: translate } = useTranslation("common");
     const [isShareSupported, setIsShareSupported] = useState(false);
 
     useEffect(() => {
-        setIsShareSupported(!!navigator.share); // Check if the Web Share API is supported
+        // Checking if the Web Share API is supported by the browser
+        setIsShareSupported(!!navigator.share);
     }, []);
 
-    const shareApp = async () => {
+    const handleShareApp = async () => {
         try {
             await navigator.share({
                 title: translate("share.title"),
                 text: translate("share.text"),
                 url: import.meta.env.VITE_URL_TO_SHARE
             });
+            // TODO: add analytics
             console.log("Successfully shared!");
         } catch (error) {
+            // TODO: add analytics
             console.error("Error sharing:", error);
         }
     };
 
     return (
         isShareSupported && (
-            <button onClick={shareApp}>{translate("share.label")}</button>
+            <button onClick={handleShareApp}>{translate("share.label")}</button>
         )
     );
 };
