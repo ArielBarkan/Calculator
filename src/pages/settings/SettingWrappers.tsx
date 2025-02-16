cf1d2216338fde9c465d9ab0421a008fea4809c8
Ariel Barkan <arielbarkan@gmail.com>
2025-02-16 21:08:31 +0200

    initial selected language

--- src/pages/settings/SettingWrappers.tsx
+++ src/pages/settings/SettingWrappers.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
-const Row = styled.div`
+const Row = styled.div<{ selected?: boolean }>`
     padding: 1rem 2rem;
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    border: 1px solid ${({ selected }) => selected ? "black" : "transparent"};
+    border-radius: 15px;
 `;
 
 export { Row };
