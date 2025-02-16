eea84d5fc2e52cda054ac822d1ae7b40a574c748
Ariel Barkan <arielbarkan@gmail.com>
2025-02-16 20:39:26 +0200

    creating TitleWithBack component + implementation

--- src/components/titleWithBack/titleWithBackWrappers.ts
+++ src/components/titleWithBack/titleWithBackWrappers.ts
@@ -8,6 +8,7 @@ import styled from "styled-components";
 const TitleWithBackWrapper = styled.div`
     display: flex;
     align-items: flex-start;
+    padding: 1.5rem 0;
 `;
 
 const Back = styled.div`
