0468afcc2710ba85db76947f0ecd9d860b434529
Ariel Barkan <arielbarkan@gmail.com>
2025-02-18 00:23:43 +0200

    start hamburger menu

--- src/components/titleWithBack/titleWithBackWrappers.ts
+++ src/components/titleWithBack/titleWithBackWrappers.ts
@@ -8,7 +8,7 @@ import styled from "styled-components";
 const TitleWithBackWrapper = styled.div`
     display: flex;
     align-items: flex-start;
-    padding: 5rem 0 0;
+    padding: 5rem 0 1rem;
 `;
 
 const Back = styled.div`
