ba4dc6ce304a7dafedbe7d5dc00bc85fe8ec3ec6
Ariel Barkan <arielbarkan@gmail.com>
2025-02-19 00:46:20 +0200

    adding option not to flip icons

--- src/styles/icons.tsx
+++ src/styles/icons.tsx
@@ -2,20 +2,16 @@
 // https://react-icons.github.io/react-icons/
 import { MdNavigateNext, MdKeyboardBackspace, MdOutlineSettings, MdShare } from "react-icons/md";
 
-
 // Custom imports
 import { IconProps } from "../types";
 import { ThemedIcon } from "../components/themedIcon/themedIcon";
 
-const IconNext = (props: IconProps) => <ThemedIcon icon={MdNavigateNext} color={props?.color}
-                                                   rtlIgnore={props?.rtlIgnore}
-                                                   size={props?.size} />;
+const IconNext = (props: IconProps) => <ThemedIcon icon={MdNavigateNext} color={props?.color} size={props?.size}
+                                                   rtlIgnore={props?.rtlIgnore} />;
 const IconBack = (props: IconProps) => <ThemedIcon icon={MdKeyboardBackspace} color={props?.color}
-                                                   rtlIgnore={props?.rtlIgnore}
-                                                   size={props?.size} />;
+                                                   rtlIgnore={props?.rtlIgnore} size={props?.size} />;
 const IconSettings = (props: IconProps) => <ThemedIcon icon={MdOutlineSettings} color={props?.color}
-                                                       rtlIgnore={props?.rtlIgnore}
-                                                       size={props?.size} />;
+                                                       rtlIgnore={props?.rtlIgnore} size={props?.size} />;
 const IconShare = (props: IconProps) => <ThemedIcon icon={MdShare} color={props?.color} rtlIgnore={props?.rtlIgnore}
                                                     size={props?.size} />;
 
