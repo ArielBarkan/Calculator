b4c8776429e38a506f6d07a85855bfae4daa3c26
Ariel Barkan <arielbarkan@gmail.com>
2025-02-21 22:02:38 +0200

    initial calculator row comp

--- src/views/pages/calculator/CalculatorPage.tsx
+++ src/views/pages/calculator/CalculatorPage.tsx
@@ -1,18 +1,25 @@
 // Modules imports
 import { useTranslation } from "react-i18next";
 import { ProductRow } from "../../../components/productRow";
+import { useState } from "react";
 
 
 const CalculatorPage = () => {
     const { t: translate } = useTranslation("common");
 
+    const initialRows: number = Number(import.meta.env.VITE_INITIAL_ROWS);
+    const maxRows: number = Number(import.meta.env.VITE_MAX_ROWS);
+
+    const [rows, setRows] = useState<number>(initialRows);
 
     return (
         <p style={{ padding: "5rem 0 0" }}>
             {translate("pages.calculator.title")}
-            <ProductRow></ProductRow>
-            <ProductRow></ProductRow>
-            <ProductRow></ProductRow>
+            {Array.from({ length: maxRows }).map((_, index) => (
+                <ProductRow key={index} />
+            ))}
+
+
         </p>
     );
 };
