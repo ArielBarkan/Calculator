dcdbacefb500d7b4f6383447381e50c844f027b8
Ariel Barkan <arielbarkan@gmail.com>
2025-02-22 22:49:26 +0200

    mechanism of add and remove rows started

--- src/components/productRow/productRow.tsx
+++ src/components/productRow/productRow.tsx
@@ -2,6 +2,7 @@ import { ProductRowContainer } from "./productRowWrappers";
 import { NumericInput } from "../numberInput";
 import { IconDelete } from "../../styles";
 import { UnstyledButton } from "../buttonUnstyled";
+import { useState } from "react";
 
 type ProductRowProps = {
     listOrder: number,
@@ -10,13 +11,23 @@ type ProductRowProps = {
 };
 
 const ProductRow = (props: ProductRowProps) => {
+    const [isFading, setIsFading] = useState(false);
+
     const { listOrder, id, deleteFunction } = props;
+
+    const handleRemoveRow = () => {
+        setIsFading(true); // Start fade-out animation
+        setTimeout(() => {
+            deleteFunction(id); // Remove row after animation completes
+        }, 300); // Match CSS transition duration
+    };
+
     return (
-        <ProductRowContainer>
+        <ProductRowContainer className={isFading ? "fade-out" : ""}>
             {listOrder + 1}
             <NumericInput />
             <NumericInput />
-            <UnstyledButton onClick={() => deleteFunction(id)}>
+            <UnstyledButton onClick={handleRemoveRow}>
                 <IconDelete size={30} />
             </UnstyledButton>
         </ProductRowContainer>
