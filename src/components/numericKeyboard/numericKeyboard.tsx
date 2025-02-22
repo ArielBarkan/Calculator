b6c3511c28e562c8d2a8f1ce87696ad8b2af0403
Ariel Barkan <arielbarkan@gmail.com>
2025-02-22 18:24:45 +0200

    initial calculator row comp

--- src/components/numericKeyboard/numericKeyboard.tsx
+++ src/components/numericKeyboard/numericKeyboard.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const KeyboardWrapper = styled.div`
     display: grid;
-    grid-template-columns: repeat(6, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     gap: 10px;
     max-width: 200px;
     margin: auto;
